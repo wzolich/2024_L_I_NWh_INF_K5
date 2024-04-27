a6116142cd4b122149f933aedead68ec546eff3f
wzolich <wzolichowski+1@gmail.com>
2024-04-27 15:56:13 +0200

    big commit fix test mgs and make test

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,3 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
